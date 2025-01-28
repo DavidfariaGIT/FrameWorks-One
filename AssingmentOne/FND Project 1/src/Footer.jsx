a408c1732d06317c36c585863954de76efc41368
DavidfariaGIT <davidfaria17@gmail.com>
2025-01-28 17:03:53 -0500

    Assignment2

---
+++ AssingmentOne/FND Project 1/src/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => { 
+    return(
+        <div>
+            <div className="footerContainer">
+                <h2>Each new assingment i will add on to this page you will continue to see this grow as the semseter goes on</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Footer; 
