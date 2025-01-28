2f356b171d812b509708acf494541f0b5d732e49
DavidfariaGIT <davidfaria17@gmail.com>
2025-01-28 16:51:40 -0500

    update

--- AssingmentOne/FND Project 1/src/Hello.jsx
+++ AssingmentOne/FND Project 1/src/Hello.jsx
@@ -15,8 +15,11 @@ function HelloMessage() {
             <div className="containerTwo">
                 <h3>I want to create a blog about the stuff i learn over the course of this semester, I will use it to showcase the skills i have learned. Then turn it into a portoflio project to display when i graduate. I plan to add to this site and keep growing it into a finish project.</h3>
             </div>
-            <img id="plant" src="public/assets/trees.jpg"></img>
-            <button><span id="highlight">David Faria</span>, Junior Front-End Developer / Designer</button>
+            <div id="profileContainer">
+                <img id="profileImg" src="public/assets/GTv8b8IW0AAj9G9.jpeg" alt="Katherine Johnson" />
+                <p>I code using a Macbook Pro that i bought last year. I have used both Mac and Windows and i just prefer the ui and design of the mac. Also something that is very important to me coming from a design background is the qaulity of things. This to me is something that apple really understands with the qaulity of its products. I used to own Dell products and i had issues with them in the past. I also have built many PC's on my own before and loved them but the lack of mobility to me is something i value as i love to work on the move.</p>
+            </div>
+            <button onClick={() => alert('Hello')}>David Faria, Junior Front-End Developer / Designer</button>
         </>
     );
 
--- AssingmentOne/FND Project 1/src/main.jsx
+++ AssingmentOne/FND Project 1/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import  HelloMessage from "./Hello"
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelloMessage/>
